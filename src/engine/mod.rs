9b106389662b9566e95bbac9227c824439cd5990
SaiSawant1 <saisawant2003@gmail.com>
2024-12-28 18:03:23 +0530

    piece rotation and position

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,8 +1,16 @@
 pub mod piece;
-use piece::Kind as PieceKind;
+use cgmath::Vector2;
+use piece::{Kind as PieceKind, Piece};
+use rand::{rngs::ThreadRng, seq::SliceRandom};
+
+type Coordinate = Vector2<usize>;
+type Offset = Vector2<isize>;
+
 pub struct Engine {
     board: Board,
     bag: Vec<PieceKind>,
+    rng: ThreadRng,
+    cursor: Option<Piece>,
 }
 
 impl Engine {
@@ -10,8 +18,16 @@ impl Engine {
         Engine {
             board: Board::blank(),
             bag: Vec::new(),
+            rng: rand::thread_rng(),
+            cursor: None,
         }
     }
+    fn fill_bag(&mut self) {
+        debug_assert!(self.bag.is_empty());
+        self.bag.extend_from_slice(&PieceKind::ALL);
+        self.bag.shuffle(&mut self.rng);
+    }
+    fn place_cursor() {}
 }
 
 struct Board {
@@ -20,8 +36,12 @@ struct Board {
 
 impl Board {
     const WIDTH: usize = 10;
-    const LENGTH: usize = 20;
-    const SIZE: usize = Board::WIDTH * Board::LENGTH;
+    const HEIGHT: usize = 20;
+    const SIZE: usize = Board::WIDTH * Board::HEIGHT;
+
+    fn in_bounds(Coordinate { x, y }: Coordinate) -> bool {
+        x < Self::WIDTH && y < Self::HEIGHT
+    }
 
     fn blank() -> Self {
         Self {
