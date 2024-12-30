fce7793b11c1392958868bfda7e42d68c9ea7c9a
SaiSawant1 <saisawant2003@gmail.com>
2024-12-30 18:05:00 +0530

    peice lock

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,4 +1,6 @@
 pub mod piece;
+use std::usize;
+
 use cgmath::Vector2;
 use piece::{Kind as PieceKind, Piece};
 use rand::{rngs::ThreadRng, seq::SliceRandom};
@@ -27,12 +29,20 @@ impl Engine {
         self.bag.extend_from_slice(&PieceKind::ALL);
         self.bag.shuffle(&mut self.rng);
     }
-    fn place_cursor() {}
+    fn place_cursor(&mut self) {
+        let cursor = self
+            .cursor
+            .take()
+            .expect("Called place_cursor Without cursor");
+        for coord in cursor.cells().expect("Cursor was outof bounds") {
+            let cell: &mut bool = self.board.get_mut(coord).unwrap();
+            debug_assert_eq!(*cell, false);
+            *cell = true;
+        }
+    }
 }
 
-struct Board {
-    cells: [bool; Board::SIZE],
-}
+struct Board([bool; Board::SIZE]);
 
 impl Board {
     const WIDTH: usize = 10;
@@ -44,8 +54,14 @@ impl Board {
     }
 
     fn blank() -> Self {
-        Self {
-            cells: [false; Self::SIZE],
-        }
+        Self([false; Self::SIZE])
+    }
+
+    fn indexing(Coordinate { x, y }: Coordinate) -> usize {
+        y * Self::WIDTH + x
+    }
+
+    fn get_mut(&mut self, coord: Coordinate) -> Option<&mut bool> {
+        Self::in_bounds(coord).then(|| &mut self.0[Self::indexing(coord)])
     }
 }
